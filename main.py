48832c64b2772855ecc4e649296d5d57e830386e
mohaned5 <mohaned@live.co.uk>
2025-01-03 03:11:13 +0000

    Attempt to manually wrap

--- main.py
+++ main.py
@@ -62,7 +62,9 @@ def cli_main():
         parser_kwargs={'parser_mode': 'omegaconf', 'default_env': True},
         seed_everything_default=os.environ.get("LOCAL_RANK", 0),
         trainer_defaults={
-            'strategy': stategy,
+            'strategy': {
+                'class_path': 'utils.fsdpstrategy.CustomFSDPStrategy'
+            },
             'log_every_n_steps': 10,
             'num_sanity_val_steps': 0,
             'limit_val_batches': 4,
