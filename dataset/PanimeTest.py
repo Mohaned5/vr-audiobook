1463041666bb22653c1a1ab67608ddfc2188bbf9
mohaned5 <mohaned@live.co.uk>
2025-01-04 01:50:44 +0000

    Fix missing param

--- dataset/PanimeTest.py
+++ dataset/PanimeTest.py
@@ -135,7 +135,7 @@ class PanimeDataModule(PanoDataModule):
 
     def __init__(
         self,
-        data_dir: str = 'data/Panime',
+        data_dir: str = 'data/PanimeT',
         **kwargs
     ):
         super().__init__(data_dir=data_dir, **kwargs)
